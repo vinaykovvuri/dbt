0854565fec2bcd778051b68d9ab2c31a6c60b7be
vinay reddy <vinayroshinikovvuri@gmail.com>
2026-05-26 09:02:17 +0000

    commit

--- models/staging/customer.sql
+++ models/staging/customer.sql
@@ -1,20 +1,5 @@
 {{ config(
-    materialized = 'table'
+    materialized = 'view'
 )}}
 
-with cte as(select * from {{source('sample_source','customer')}}),
-
-renamed as(
-select
-c_custkey as cust_key,
-c_name as name,
-c_address as address,
-c_nationkey as nationkey,
-c_phone as phone,
-c_acctbal as acctbal,
-c_mktsegment as mktsegment,
-c_comment as comment
-from cte
-)
-
-select * from renamed
+select * from {{ source('dbt_pro_1','raw_customers')}}
